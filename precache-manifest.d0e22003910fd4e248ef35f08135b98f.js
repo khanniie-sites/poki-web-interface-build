0d10a6770f769b8c8ceab8c3e728721852ccfc14
Connie Ye <constanceye1316@gmail.com>
2020-03-22 04:26:35 -0400

    build

---
+++ precache-manifest.d0e22003910fd4e248ef35f08135b98f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "346f7fa0ce2165ce3bebcc8c6d89aea7",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5458da86fdb6a3850ee5",
+    "url": "/static/css/main.e1d94154.chunk.css"
+  },
+  {
+    "revision": "483ae8d162dfce618211",
+    "url": "/static/js/2.12543479.chunk.js"
+  },
+  {
+    "revision": "f65b45f562155f0fa2653b346e3552a6",
+    "url": "/static/js/2.12543479.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5458da86fdb6a3850ee5",
+    "url": "/static/js/main.84ec03e0.chunk.js"
+  },
+  {
+    "revision": "415e9bcf6c081ea50565",
+    "url": "/static/js/runtime-main.f8c5b4be.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
